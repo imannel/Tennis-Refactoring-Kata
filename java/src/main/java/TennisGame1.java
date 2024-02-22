08bcfffc23c9e742cd92bd9672421c4d48e5364c
imannel <afamanalafa2001@gmail.com>
2024-02-22 13:27:47 +0100

    afa_manal_tp_clean_code

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,12 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
+
+
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,65 +14,70 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name))
+            player1Score += 1;
+        if (playerName.equals(player2Name))
+            player2Score += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        int tempScore = 0;
+        if (player1Score == player2Score) {
+            switch (player1Score) {
                 case 0:
-                        score = "Love-All";
-                    break;
+                    return score = "Love-All";
+
                 case 1:
-                        score = "Fifteen-All";
-                    break;
+                    return score = "Fifteen-All";
+
                 case 2:
-                        score = "Thirty-All";
-                    break;
+                    return score = "Thirty-All";
+
                 default:
-                        score = "Deuce";
-                    break;
-                
+                    return score = "Deuce";
+
+
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (player1Score >= 4 || player2Score >= 4) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) return score = "Advantage " + player1Name;
+            else if (minusResult == -1) return score = "Advantage "  + player2Name;
+            else if (minusResult >= 2) return score = "Win for "  +player1Name;
+            return  score = "Win for "+ player2Name;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+
+                switch ( player1Score) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
-            }
-        }
+                switch ( player2Score) {
+                    case 0:
+                        score += "-Love";
+                        break;
+                    case 1:
+                        score += "-Fifteen";
+                        break;
+                    case 2:
+                        score += "-Thirty";
+                        break;
+                    case 3:
+                        score += "-Forty";
+                        break;
+                }
+
+
         return score;
     }
 }
